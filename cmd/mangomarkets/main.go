0d096adc2256a9213411ab95d41ca8b2f392bb02
mango <menziesjustin@ymail.com>
2024-02-16 18:52:51 -0500

    - implemented bubble tea basics

--- cmd/mangomarkets/main.go
+++ cmd/mangomarkets/main.go
@@ -1,46 +1,40 @@
 // main.go
 package main
 
-import (
-	"fmt"
-
-	"github.com/jp-mango/mangomarkets/internal/api"
-	"github.com/jp-mango/mangomarkets/internal/config"
-	"github.com/jp-mango/mangomarkets/util"
-)
+import "github.com/jp-mango/mangomarkets/pkg/charm"
 
 func main() {
-	// charm.Start()
-	apiKey := config.LoadEnv()
-
-	var ticker string
-	fmt.Print("Enter ticker: ")
-	fmt.Scanln(&ticker)
-
-	var interval int
-	fmt.Println("Time interval?\n [1]:daily  [2]:weekly  [3]monthly")
-	fmt.Scanln(&interval)
-
-	switch interval {
-	case 1:
-		tsDataDaily, err := api.FetchTimeSeriesDaily(apiKey, ticker)
-		if err != nil {
-			fmt.Print("Unable to load daily time series data for", ticker)
-		}
-		util.PrintTimeSeriesData(tsDataDaily)
-	case 2:
-		tsDataWeekly, err := api.FetchTimeSeriesWeekly(apiKey, ticker)
-		if err != nil {
-			fmt.Print("Unable to load daily time series data for", ticker)
-		}
-		util.PrintTimeSeriesData(tsDataWeekly)
-	case 3:
-		tsDataMonthly, err := api.FetchTimeSeriesMonthly(apiKey, ticker)
-		if err != nil {
-			fmt.Print("Unable to load daily time series data for", ticker)
-		}
-		util.PrintTimeSeriesData(tsDataMonthly)
-	default:
-		fmt.Println("Invalid interval")
-	}
+	charm.Start()
+	//apiKey := config.LoadEnv()
+	//
+	//var ticker string
+	//fmt.Print("Enter ticker: ")
+	//fmt.Scanln(&ticker)
+	//
+	//var interval int
+	//fmt.Println("Time interval?\n [1]:daily  [2]:weekly  [3]monthly")
+	//fmt.Scanln(&interval)
+	//
+	//switch interval {
+	//case 1:
+	//	tsDataDaily, err := api.FetchTimeSeriesDaily(apiKey, ticker)
+	//	if err != nil {
+	//		fmt.Print("Unable to load daily time series data for", ticker)
+	//	}
+	//	util.PrintTimeSeriesData(tsDataDaily)
+	//case 2:
+	//	tsDataWeekly, err := api.FetchTimeSeriesWeekly(apiKey, ticker)
+	//	if err != nil {
+	//		fmt.Print("Unable to load daily time series data for", ticker)
+	//	}
+	//	util.PrintTimeSeriesData(tsDataWeekly)
+	//case 3:
+	//	tsDataMonthly, err := api.FetchTimeSeriesMonthly(apiKey, ticker)
+	//	if err != nil {
+	//		fmt.Print("Unable to load daily time series data for", ticker)
+	//	}
+	//	util.PrintTimeSeriesData(tsDataMonthly)
+	//default:
+	//	fmt.Println("Invalid interval")
+	//}
 }
