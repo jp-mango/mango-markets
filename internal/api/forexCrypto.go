e0e212e345fdfb7626d3cdd0dd4e73591c73def3
mango <menziesjustin@ymail.com>
2024-02-29 18:12:26 -0500

    -started forex time series data feature

--- internal/api/forexCrypto.go
+++ internal/api/forexCrypto.go
@@ -1,5 +1,14 @@
 package api
 
-func FetchExchangeRates(base, new string) {
+type ForexTimeSeriesData interface {
+	GetTimeSeriesData() map[string]map[string]string
+}
 
+type ForexMetaData struct {
+	Information   string `json:"1. Information"`
+	FromSymbol    string `json:"2. From Symbol"`
+	ToSymbol      string `json:"3. To Symbol"`
+	OutputSize    string `json:"4. Output Size"`
+	LastRefreshed string `json:"5. Last Refreshed"`
+	TimeZone      string `json:"6. Time Zone"`
 }
