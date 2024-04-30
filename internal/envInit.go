f15d52b81c5dd61396b9e485e28d6388b222ecb3
jp-mango <menziesjustin@ymail.com>
2024-04-30 16:41:28 -0400

    - moved env file loading

---
+++ internal/envInit.go
@@ -0,0 +1,16 @@
+package internal
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return err
+}
