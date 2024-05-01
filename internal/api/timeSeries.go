556f20223d14ab71aecac72b8eed9524564eb620
mango <menziesjustin@ymail.com>
2024-05-01 15:38:03 -0400

    - finished up gathering of timeSeries data

--- internal/api/timeSeries.go
+++ internal/api/timeSeries.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 type TimeSeriesData struct {
 	Metadata   Metadata              `json:"Meta Data"`
-	TimeSeries map[string]StockPrice `json:"Time Series"`
+	TimeSeries map[string]StockPrice `json:"-"`
 	Frequency  string
 }
 
@@ -23,38 +24,92 @@ type Metadata struct {
 }
 
 type StockPrice struct {
-	Open           float64 `json:"1. open"`
-	High           float64 `json:"2. high"`
-	Low            float64 `json:"3. low"`
-	Close          float64 `json:"4. close"`
-	Volume         int64   `json:"5. volume"`
-	AdjustedClose  float64 `json:"5. adjusted close"`
-	DividendAmount float64 `json:"7. dividend amount"`
+	Open   string `json:"1. open"`
+	High   string `json:"2. high"`
+	Low    string `json:"3. low"`
+	Close  string `json:"4. close"`
+	Volume string `json:"5. volume"`
 }
 
-func intradayData(apiKey, ticker, interval string) (TimeSeriesData, error) {
-	/*
-	*	- Interval (1m,5m,15m,30m,60m)
-	 */
-	url := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=%s&entitlement=delayed&apikey=%s", ticker, interval, apiKey)
+// ! Custom unmarshal to handle dynamic json requests
+func (t *TimeSeriesData) UnmarshalJSON(data []byte) error {
+	var raw map[string]json.RawMessage
+	err := json.Unmarshal(data, &raw)
+	if err != nil {
+		return err
+	}
 
+	// Unmarshal Metadata
+	err = json.Unmarshal(raw["Meta Data"], &t.Metadata)
+	if err != nil {
+		return err
+	}
+
+	// Unmarshal TimeSeries
+	for key, value := range raw {
+		if strings.HasPrefix(key, "Time Series") {
+			err = json.Unmarshal(value, &t.TimeSeries)
+			if err != nil {
+				return err
+			}
+			t.Frequency = strings.TrimPrefix(key, "Time Series (")
+			t.Frequency = strings.TrimSuffix(t.Frequency, ")")
+			break
+		} else if strings.HasSuffix(key, "Time Series") {
+			err = json.Unmarshal(value, &t.TimeSeries)
+			if err != nil {
+				return err
+			}
+			t.Frequency = strings.TrimSuffix(key, " Time Series")
+			break
+		}
+	}
+
+	return nil
+}
+
+// ! Pull the data from the api
+func dataPull(url string) (*TimeSeriesData, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return TimeSeriesData{}, fmt.Errorf("no info returned from url: %s", err)
+		return nil, fmt.Errorf("no info returned from url: %s", err)
 	}
 	defer resp.Body.Close()
 
 	content, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return TimeSeriesData{}, fmt.Errorf("error reading response: %s", err)
+		return nil, fmt.Errorf("error reading response: %s", err)
 	}
 
 	var data TimeSeriesData
 
 	err = json.Unmarshal(content, &data)
 	if err != nil {
-		return TimeSeriesData{}, fmt.Errorf("error unmarshaling JSON: %s", err)
+		return nil, fmt.Errorf("error unmarshaling JSON: %s", err)
 	}
 
-	return data, nil
+	return &data, nil
+}
+
+func IntradayDataPull(apiKey, ticker, interval string) (*TimeSeriesData, error) {
+	/*
+	*	- Interval (1m,5m,15m,30m,60m)
+	 */
+	url := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=%s&entitlement=delayed&apikey=%s", ticker, interval, apiKey)
+	return dataPull(url)
+}
+
+func DailyDataPull(apiKey, ticker string) (*TimeSeriesData, error) {
+	url := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&apikey=%s", ticker, apiKey)
+	return dataPull(url)
+}
+
+func WeeklyDataPull(apiKey, ticker string) (*TimeSeriesData, error) {
+	url := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=%s&apikey=%s", ticker, apiKey)
+	return dataPull(url)
+}
+
+func MonthlyDataPull(apiKey, ticker string) (*TimeSeriesData, error) {
+	url := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=%s&apikey=%s", ticker, apiKey)
+	return dataPull(url)
 }
