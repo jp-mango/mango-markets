aa819894da935899a968e4675da7fd93632e3452
mango <menziesjustin@ymail.com>
2024-05-01 15:39:09 -0400

    - tested stock price pull

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"mangomarkets/internal"
+	"mangomarkets/internal/api"
 )
 
 func main() {
-	internal.LoadEnv()
+	apiKey, _ := internal.LoadEnv()
+
+	intradayStockInfo, err := api.IntradayDataPull(apiKey, "AAPL", "5min")
+	if err != nil {
+		fmt.Println("broken", err)
+	}
+
+	dailyStockInfo, err := api.DailyDataPull(apiKey, "AAPL")
+	if err != nil {
+		fmt.Println("broken", err)
+	}
+
+	weeklyStockInfo, err := api.WeeklyDataPull(apiKey, "NVDA")
+	if err != nil {
+		fmt.Println("broken", err)
+	}
+
+	monthlyStockInfo, err := api.MonthlyDataPull(apiKey, "NVDA")
+	if err != nil {
+		fmt.Println("broken", err)
+	}
+
+	fmt.Println(intradayStockInfo.TimeSeries)
+	fmt.Print("\n\n\n\n\n\n\n")
+	fmt.Println(dailyStockInfo.TimeSeries)
+	fmt.Print("\n\n\n\n\n\n\n")
+	fmt.Println(weeklyStockInfo.TimeSeries)
+	fmt.Print("\n\n\n\n\n\n\n")
+	fmt.Println(monthlyStockInfo.TimeSeries)
+
 }
