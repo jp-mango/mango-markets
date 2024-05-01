c4b511fd908be2715a1771cc7e2ff5e1cefc75ba
mango <menziesjustin@ymail.com>
2024-05-01 18:01:17 -0400

    - tested most active tickers

--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,52 @@
 package main
 
-import (
-	"fmt"
-	"mangomarkets/internal"
-	"mangomarkets/internal/api"
-)
-
 func main() {
-	apiKey, _ := internal.LoadEnv()
+	//apiKey, _ := internal.LoadEnv()
+	/*
+			intradayStockInfo, err := api.IntradayDataPull(apiKey, "AAPL", "5min")
+			if err != nil {
+				fmt.Println("intraday broken", err)
+			}
 
-	intradayStockInfo, err := api.IntradayDataPull(apiKey, "AAPL", "5min")
-	if err != nil {
-		fmt.Println("broken", err)
-	}
+			dailyStockInfo, err := api.DailyDataPull(apiKey, "AAPL")
+			if err != nil {
+				fmt.Println("daily broken", err)
+			}
 
-	dailyStockInfo, err := api.DailyDataPull(apiKey, "AAPL")
-	if err != nil {
-		fmt.Println("broken", err)
-	}
+			weeklyStockInfo, err := api.WeeklyDataPull(apiKey, "NVDA")
+			if err != nil {
+				fmt.Println("weekly broken", err)
+			}
 
-	weeklyStockInfo, err := api.WeeklyDataPull(apiKey, "NVDA")
-	if err != nil {
-		fmt.Println("broken", err)
-	}
+			monthlyStockInfo, err := api.MonthlyDataPull(apiKey, "NVDA")
+			if err != nil {
+				fmt.Println("monthly broken", err)
+			}
 
-	monthlyStockInfo, err := api.MonthlyDataPull(apiKey, "NVDA")
-	if err != nil {
-		fmt.Println("broken", err)
-	}
+			fmt.Println(intradayStockInfo.TimeSeries)
+			fmt.Print("\n\n\n\n\n\n\n")
+			fmt.Println(dailyStockInfo.TimeSeries)
+			fmt.Print("\n\n\n\n\n\n\n")
+			fmt.Println(weeklyStockInfo.TimeSeries)
+			fmt.Print("\n\n\n\n\n\n\n")
+			fmt.Println(monthlyStockInfo.TimeSeries)
+			fmt.Print("\n\n\n\n\n\n\n")
 
-	fmt.Println(intradayStockInfo.TimeSeries)
-	fmt.Print("\n\n\n\n\n\n\n")
-	fmt.Println(dailyStockInfo.TimeSeries)
-	fmt.Print("\n\n\n\n\n\n\n")
-	fmt.Println(weeklyStockInfo.TimeSeries)
-	fmt.Print("\n\n\n\n\n\n\n")
-	fmt.Println(monthlyStockInfo.TimeSeries)
-	fmt.Print("\n\n\n\n\n\n\n")
+		news, err := api.FetchNewsSentimentData(apiKey, "NVDA")
+		if err != nil {
+			fmt.Println("news broken", err)
+		}
 
-	news, err := api.FetchNewsSentimentData(apiKey, "NVDA")
-	fmt.Println(news.Feed)
+		for _, inf := range news.Feed {
+			fmt.Printf("|%s|\n- %s (%s)\n", inf.Title, inf.Summary, inf.URL)
+		}
+
+			gainLoss, err := api.FetchTopGainLossData(apiKey)
+			if err != nil {
+				fmt.Println("gain loss broken", err)
+			}
+			for _, active := range gainLoss.MostActive {
+				fmt.Printf("%s (%s): %s| ",active, active.Ticker, active.Volume)
+			}
+	*/
 }
