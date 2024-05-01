0eb956c265771d7015056f3d74122e84df0348e2
mango <menziesjustin@ymail.com>
2024-05-01 15:38:33 -0400

    - reconfigured returns

--- internal/envInit.go
+++ internal/envInit.go
@@ -2,15 +2,18 @@ package internal
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func LoadEnv() error {
-	err := godotenv.Load()
+func LoadEnv() (API_KEY string, err error) {
+	err = godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	return err
+	API_KEY = os.Getenv("API_KEY")
+
+	return API_KEY, err
 }
