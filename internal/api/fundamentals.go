68d20308584da04b24088f8b1347c0e3d884eaff
mango <menziesjustin@ymail.com>
2024-05-01 18:44:57 -0400

    - implemented company fundamentals

---
+++ internal/api/fundamentals.go
@@ -0,0 +1,79 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type CompanyInfo struct {
+	Symbol                     string `json:"Symbol"`
+	AssetType                  string `json:"AssetType"`
+	Name                       string `json:"Name"`
+	Description                string `json:"Description"`
+	CIK                        string `json:"CIK"`
+	Exchange                   string `json:"Exchange"`
+	Currency                   string `json:"Currency"`
+	Country                    string `json:"Country"`
+	Sector                     string `json:"Sector"`
+	Industry                   string `json:"Industry"`
+	Address                    string `json:"Address"`
+	FiscalYearEnd              string `json:"FiscalYearEnd"`
+	LatestQuarter              string `json:"LatestQuarter"`
+	MarketCapitalization       string `json:"MarketCapitalization"`
+	EBITDA                     string `json:"EBITDA"`
+	PERatio                    string `json:"PERatio"`
+	PEGRatio                   string `json:"PEGRatio"`
+	BookValue                  string `json:"BookValue"`
+	DividendPerShare           string `json:"DividendPerShare"`
+	DividendYield              string `json:"DividendYield"`
+	EPS                        string `json:"EPS"`
+	RevenuePerShareTTM         string `json:"RevenuePerShareTTM"`
+	ProfitMargin               string `json:"ProfitMargin"`
+	OperatingMarginTTM         string `json:"OperatingMarginTTM"`
+	ReturnOnAssetsTTM          string `json:"ReturnOnAssetsTTM"`
+	ReturnOnEquityTTM          string `json:"ReturnOnEquityTTM"`
+	RevenueTTM                 string `json:"RevenueTTM"`
+	GrossProfitTTM             string `json:"GrossProfitTTM"`
+	DilutedEPSTTM              string `json:"DilutedEPSTTM"`
+	QuarterlyEarningsGrowthYOY string `json:"QuarterlyEarningsGrowthYOY"`
+	QuarterlyRevenueGrowthYOY  string `json:"QuarterlyRevenueGrowthYOY"`
+	AnalystTargetPrice         string `json:"AnalystTargetPrice"`
+	AnalystRatingStrongBuy     string `json:"AnalystRatingStrongBuy"`
+	AnalystRatingBuy           string `json:"AnalystRatingBuy"`
+	AnalystRatingHold          string `json:"AnalystRatingHold"`
+	AnalystRatingSell          string `json:"AnalystRatingSell"`
+	AnalystRatingStrongSell    string `json:"AnalystRatingStrongSell"`
+	TrailingPE                 string `json:"TrailingPE"`
+	ForwardPE                  string `json:"ForwardPE"`
+	PriceToSalesRatioTTM       string `json:"PriceToSalesRatioTTM"`
+	PriceToBookRatio           string `json:"PriceToBookRatio"`
+	EVToRevenue                string `json:"EVToRevenue"`
+	EVToEBITDA                 string `json:"EVToEBITDA"`
+	Beta                       string `json:"Beta"`
+	FiftyTwoWeekHigh           string `json:"52WeekHigh"`
+	FiftyTwoWeekLow            string `json:"52WeekLow"`
+	FiftyDayMovingAverage      string `json:"50DayMovingAverage"`
+	TwoHundredDayMovingAverage string `json:"200DayMovingAverage"`
+	SharesOutstanding          string `json:"SharesOutstanding"`
+	DividendDate               string `json:"DividendDate"`
+	ExDividendDate             string `json:"ExDividendDate"`
+}
+
+func FetchCompanyInfo(ticker, apiKey string) (*CompanyInfo, error) {
+	url := fmt.Sprintf("https://www.alphavantage.co/query?function=OVERVIEW&symbol=%s&apikey=%s", ticker, apiKey)
+
+	var companyInfo CompanyInfo
+
+	data, err := DataPull(url)
+	if err != nil {
+		return nil, fmt.Errorf("err: %s", err)
+	}
+
+	err = json.Unmarshal(data, &companyInfo)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON response: %v", err)
+
+	}
+
+	return &companyInfo, nil
+}
