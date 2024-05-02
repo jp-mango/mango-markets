ab49e7c1583f62026804d199417dc9ff1a052fc8
mango <menziesjustin@ymail.com>
2024-05-02 13:11:39 -0400

    - TODO: use active listings to populate relevant data

--- internal/api/activeListings.go
+++ internal/api/activeListings.go
@@ -1,1 +1,17 @@
 package api
+
+import (
+	"fmt"
+	"mangomarkets/internal"
+)
+
+func FetchActiveListings(apiKey string) (string, error) {
+	url := fmt.Sprintf("https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=%s", apiKey)
+
+	listings, err := DataPull(url)
+	if err != nil {
+		return "", internal.ErrDataPull(err)
+	}
+
+	return string(listings), nil
+}
