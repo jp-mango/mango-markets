bea81a9b551c2b3e48f41eeffa7fc1b5bcdfc453
mango <menziesjustin@ymail.com>
2024-05-02 13:31:32 -0400

    - added comments

--- internal/errors.go
+++ internal/errors.go
@@ -2,10 +2,12 @@ package internal
 
 import "fmt"
 
+// Returns an error if http request or reading the response body of request fails
 func ErrDataPull(err error) error {
 	return fmt.Errorf("error pulling data: %v", err)
 }
 
+// Returns an error if unmarshalling json into struct fails
 func ErrUnmarshalJSON(err error) error {
 	return fmt.Errorf("failed to unmarshal JSON response: %v", err)
 }
