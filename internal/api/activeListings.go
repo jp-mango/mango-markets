743567b94057514bac282c3035575c128399552d
mango <menziesjustin@ymail.com>
2024-05-02 13:28:47 -0400

    - added comment

--- internal/api/activeListings.go
+++ internal/api/activeListings.go
@@ -5,6 +5,10 @@ import (
 	"mangomarkets/internal"
 )
 
+/*
+! Listing Status:
+This API returns a list of active or delisted US stocks and ETFs, either as of the latest trading day or at a specific time in history. The endpoint is positioned to facilitate equity research on asset lifecycle and survivorship.
+*/
 func FetchActiveListings(apiKey string) (string, error) {
 	url := fmt.Sprintf("https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=%s", apiKey)
 
