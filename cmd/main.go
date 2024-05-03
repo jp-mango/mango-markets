7836161a01b906a56b3b7c793eefa93b253f82e4
Justin Menzies <menziesjustin@ymail.com>
2024-05-02 22:14:44 -0400

    - pulled db connection into seperate function. Implented slog

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"fmt"
+	"log/slog"
 	"mangomarkets/internal"
+	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	_, DB_CONN, _ := internal.LoadEnv()
-
+func openDB(DB_CONN string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", DB_CONN)
 	if err != nil {
-		fmt.Println("Error opening db:", err)
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func main() {
+	_, DB_CONN, _ := internal.LoadEnv()
+	
+	logger := slog.New(slog.NewTextHandler(os.Stdout,nil))
+
+	db, err := openDB(DB_CONN)
+	if err != nil{
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
 	defer db.Close()
-
-	_, err = db.Exec(`INSERT INTO test(name, age) VALUES($1, $2)`, "jp-mango", 26)
-	if err != nil {
-		fmt.Println("Error executing INSERT:", err)
-		return
-	}
-
-	fmt.Println("Data inserted successfully")
 
 }
---
+++ queries.sql
@@ -0,0 +1,3 @@
+INSERT INTO daily_stock_data
+(ticker, price_data,high,low,close,volume) 
+VALUES($1, $2, $3, $4,$5,$6)
