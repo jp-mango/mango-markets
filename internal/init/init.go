e2c0cbce2d24ba3f685a4d71881538e623eb8747
mango <menziesjustin@ymail.com>
2024-05-03 11:44:01 -0400

    - moved initiations into 1 file

---
+++ internal/init/init.go
@@ -0,0 +1,58 @@
+package init
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadLogging() (*slog.Logger, *os.File, error) {
+	logLocation, err := os.OpenFile(`./log_records/logs.jsonl`, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error opening log file: %v", err)
+	}
+
+	multi := io.MultiWriter(logLocation, os.Stdout)
+
+	logger := slog.New(slog.NewJSONHandler(multi, nil))
+	slog.SetDefault(logger)
+	return logger, logLocation, nil
+}
+
+func LoadDB(DB_CONN string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", DB_CONN)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func LoadEnv() (API_KEY, DB_CONN string, err error) {
+	err = godotenv.Load()
+	//* (UNCOMMENT FOR DEBUGGING) //err = godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	API_KEY = os.Getenv("API_KEY")
+	DB_CONN = os.Getenv("DB_DSN")
+
+	return API_KEY, DB_CONN, err
+}
