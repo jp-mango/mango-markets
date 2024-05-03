a91127a0841908562148169e5bf10dd896f19300
mango <menziesjustin@ymail.com>
2024-05-03 17:18:02 -0400

    - tested and implemented error handling for current set of functions

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"mangomarkets/internal/api"
 	"mangomarkets/internal/load"
 
 	_ "github.com/lib/pq"
@@ -19,7 +20,7 @@ func main() {
 	defer logFile.Close()
 
 	//! Load env variables
-	_, DB_CONN, err := load.Env()
+	apiKey, DB_CONN, err := load.Env()
 	if err != nil {
 		logger.Error(err.Error())
 	}
@@ -33,5 +34,64 @@ func main() {
 	defer db.Close()
 
 	//! bizniz logic
+	/*
+		* Time series data
+			intradayData, err := api.FetchIntradayTSData(apiKey, "AMD", "30min")
+			if err != nil || intradayData == nil {
+				logger.Error(err.Error())
+			}
+
+			dailyData, err := api.FetchDailyTSData(apiKey, "AMD")
+			if err != nil || dailyData == nil {
+				logger.Error(err.Error())
+			}
+
+			weeklyData, err := api.FetchWeeklyTSData(apiKey, "AMD")
+			if err != nil || weeklyData == nil {
+				logger.Error(err.Error())
+			}
+
+			monthlyData, err := api.FetchMonthlyTSData(apiKey, "AMD")
+			if err != nil || monthlyData == nil {
+				logger.Error(err.Error())
+			}
+	*/
+
+	/*
+				* Fundamental data
+				companyInfo, err := api.FetchCompanyInfo("AMD", apiKey)
+				if err != nil || companyInfo == nil {
+					logger.Error(err.Error())
+				} else {
+					fmt.Println(companyInfo)
+				}
+
+				incomeStatement, err := api.FetchIncomeStatement("AMD", apiKey)
+				if err != nil || incomeStatement == nil {
+					logger.Error(err.Error())
+				} else {
+					fmt.Println(incomeStatement)
+				}
+
+			balanceSheet, err := api.FetchBalanceSheet("NVDA", apiKey)
+			if err != nil || balanceSheet == nil {
+				logger.Error(err.Error())
+			} else {
+				fmt.Println(balanceSheet)
+			}
+
+		cashFlow, err := api.FetchCashFlow("AAPL", apiKey)
+		if err != nil || cashFlow == nil {
+			logger.Error(err.Error())
+		} else {
+			fmt.Println(cashFlow)
+		}
+	*/
+	earnings, err := api.FetchEarnings("AAPL", apiKey)
+	if err != nil || earnings == nil {
+		logger.Error(err.Error())
+	} else {
+		fmt.Println(earnings)
+	}
 
 }
