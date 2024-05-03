0ea29a02bfc409323e5d760e15374f052c391417
mango <menziesjustin@ymail.com>
2024-05-03 13:29:09 -0400

    - tested new changes

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	"mangomarkets/internal/api"
 	"mangomarkets/internal/load"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	// Set up logging
+	//! Set up logging
 	logger, logFile, err := load.Logging()
 	if err != nil {
 		fmt.Println(fmt.Errorf("error loading logging: %v", err))
@@ -18,13 +19,13 @@ func main() {
 	}
 	defer logFile.Close()
 
-	// Load env variables
-	_, DB_CONN, err := load.Env()
+	//! Load env variables
+	apiKey, DB_CONN, err := load.Env()
 	if err != nil {
 		logger.Error(err.Error())
 	}
 
-	// Connect to DB
+	//! Connect to DB
 	db, err := load.DB(DB_CONN)
 	if err != nil {
 		logger.Error(err.Error())
@@ -32,6 +33,28 @@ func main() {
 	}
 	defer db.Close()
 
-	//bizniz logic
+	//! bizniz logic
+	// Daily
+	dailyData, err := api.FetchDailyTSData(apiKey, "AAPL")
+	if err != nil || dailyData == nil {
+		logger.Error(err.Error())
+	} else {
+		fmt.Println(api.PrintTimeSeries(dailyData))
+	}
 
+	// Weekly
+	weeklyData, err := api.FetchWeeklyTSData(apiKey, "AMD")
+	if err != nil || weeklyData == nil {
+		logger.Error(err.Error())
+	} else {
+		fmt.Println(api.PrintTimeSeries(weeklyData))
+	}
+
+	// Monthly
+	monthlyData, err := api.FetchMonthlyTSData(apiKey, "NVDA")
+	if err != nil || monthlyData == nil {
+		logger.Error(err.Error())
+	} else {
+		fmt.Println(api.PrintTimeSeries(monthlyData))
+	}
 }
